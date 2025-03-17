94ec297727a63d11ce2fb5c4c5d24b51a7e3b268
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-03-17 15:20:35 +0000

    добавил схему для инициализации БД

---
+++ schema/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS users (
+    telegram_id BIGINT PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    subscription_active BOOLEAN DEFAULT FALSE,
+    subscription_expiry TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS devices (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT REFERENCES users(telegram_id) ON DELETE CASCADE,
+    private_key TEXT NOT NULL,
+    public_key TEXT NOT NULL,
+    ip TEXT NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE
+);
