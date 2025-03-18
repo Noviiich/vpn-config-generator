d0d13878034b04b3c6f7fe131177c6d9fae17798
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-03-18 15:28:44 +0000

    обновил интерфейсы

--- storage/storage.go
+++ storage/storage.go
@@ -1,12 +1,17 @@
 package storage
 
-import "time"
+import (
+	"context"
+	"time"
+)
 
 type Storage interface {
 	InitDB()
 	CreateDevice(device *Device) error
 	CreateUser(user User) error
 	IsExistsUser(telegramID int) (bool, error)
+	GetIP(ctx context.Context) (string, error)
+	UpdateIP(ctx context.Context, newIP string) error
 }
 
 type User struct {
