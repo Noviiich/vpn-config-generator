aebec81d6ccb959851b2250e679f26f127abc06f
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-03-27 15:22:20 +0000

    начал изменят БД для масштабирования

--- schema/init.sql
+++ schema/init.sql
@@ -1,20 +1,31 @@
 CREATE TABLE IF NOT EXISTS users (
-telegram_id BIGINT PRIMARY KEY,
-username TEXT UNIQUE NOT NULL
+    telegram_id BIGINT PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS devices (
     id SERIAL PRIMARY KEY,
-    user_id BIGINT REFERENCES users(telegram_id) ON DELETE CASCADE,
-    private_key TEXT NOT NULL,
-    public_key TEXT NOT NULL,
-    ip TEXT NOT NULL,
-    is_active BOOLEAN DEFAULT TRUE
+    user_id BIGINT NOT NULL REFERENCES users(telegram_id) ON DELETE CASCADE,
+    private_key TEXT NOT NULL CHECK (length(private_key) > 10),
+    public_key TEXT NOT NULL CHECK (length(private_key) > 10),
+    ip_id INET UNIQUE NOT NULL REFERENCES ip_pool(id) ON DELETE RESTRICT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
 );
 
 CREATE TABLE IF NOT EXISTS ip_pool (
 	id SERIAL PRIMARY KEY,
-	last_ip TEXT NOT NULL
+    ip inet UNIQUE NOT NULL,
+    network CIDR NOT NULL UNIQUE,
+    last_assigned_ip INET NOT NULL, 
+);
+
+CREATE TABLE IF NOT EXISTS subscription_types (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    duration INTERVAL NOT NULL,
+    max_devices INT NOT NULL DEFAULT 3
 );
 
 CREATE TABLE IF NOT EXISTS subscriptions (
