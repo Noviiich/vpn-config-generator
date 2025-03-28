88f4b1c28ef9321a6404b04579674e452cea780d
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-03-28 16:14:11 +0000

    усовершенствовал архитектуру БД

--- schema/init.sql
+++ schema/init.sql
@@ -1,25 +1,28 @@
 CREATE TABLE IF NOT EXISTS users (
-    telegram_id BIGINT PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    telegram_id BIGINT UNIQUE NOT NULL,
     username TEXT UNIQUE NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE IF NOT EXISTS devices (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users(telegram_id) ON DELETE CASCADE,
-    private_key TEXT NOT NULL CHECK (length(private_key) > 10),
-    public_key TEXT NOT NULL CHECK (length(private_key) > 10),
-    ip_id INET UNIQUE NOT NULL REFERENCES ip_pool(id) ON DELETE RESTRICT,
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-);
-
 CREATE TABLE IF NOT EXISTS ip_pool (
 	id SERIAL PRIMARY KEY,
-    ip inet UNIQUE NOT NULL,
-    network CIDR NOT NULL UNIQUE,
-    last_assigned_ip INET NOT NULL, 
+    device_id INT REFERENCES devices(id) ON DELETE SET NULL,
+    ip INET UNIQUE NOT NULL,
+    is_available BOOLEAN GENERATED ALWAYS AS (device_id IS NULL) STORED
 );
+
+CREATE TABLE IF NOT EXISTS devices (
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    private_key TEXT NOT NULL CHECK (length(private_key) > 10),
+    public_key TEXT NOT NULL CHECK (length(public_key) > 10),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    last_active TIMESTAMP,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE
+);
+
 
 CREATE TABLE IF NOT EXISTS subscription_types (
     id SERIAL PRIMARY KEY,
@@ -29,7 +32,9 @@ CREATE TABLE IF NOT EXISTS subscription_types (
 );
 
 CREATE TABLE IF NOT EXISTS subscriptions (
-    user_id BIGINT PRIMARY KEY REFERENCES users(telegram_id) ON DELETE CASCADE,
+    user_id BIGINT PRIMARY KEY REFERENCES users(id) ON DELETE CASCADE,
+    type_id INT NOT NULL REFERENCES subscription_types(id),
+    start_date TIMESTAMP NOT NULL DEFAULT NOW(),
     expiry_date TIMESTAMP DEFAULT NULL,
     is_active BOOLEAN DEFAULT FALSE
 );
