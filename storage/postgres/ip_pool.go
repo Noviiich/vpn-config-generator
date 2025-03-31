46895f34b9c271a55d1dd023196fc5ba1a484c7f
Alex Novikov <150938218+Noviiich@users.noreply.github.com>
2025-03-31 16:10:22 +0000

    методы для работу с ip_pool

--- storage/postgres/ip_pool.go
+++ storage/postgres/ip_pool.go
@@ -1,5 +1,11 @@
 package postgres
 
+import (
+	"context"
+
+	"github.com/Noviiich/vpn-config-generator/storage"
+)
+
 // func (s *Storage) GetIP(ctx context.Context, deviceID int) error {
 // 	query := `SELECT INTO ip_pool (last_ip) VALUES ($1)`
 // 	_, err := s.db.ExecContext(ctx, query, newIP)
@@ -28,3 +34,60 @@ package postgres
 
 // 	return lastIP, nil
 // }
+
+func (s *Storage) GetIpIsNull(ctx context.Context) (*storage.IPAddress, error) {
+	query := `
+			SELECT * FROM ip_pool
+			WHERE device_id IS NULL
+			LIMIT 1`
+	var ip storage.IPAddress
+	err := s.db.GetContext(ctx, &ip, query)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ip, nil
+}
+
+func (s *Storage) CreateIP(ctx context.Context, IPAdderss *storage.IPAddress) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// Откат транзакции в случае ошибки
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+	query := `
+			INSERT INTO ip_pool (device_id, ip, is_available)
+			VALUES ($1, $2, true)
+			RETURNING id`
+	err = tx.QueryRowContext(ctx, query, IPAdderss.DeviceID, IPAdderss.IP).Scan(&IPAdderss.ID)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func (s *Storage) UpdateIpPool(ctx context.Context, id string) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// Откат транзакции в случае ошибки
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
+
+	query := `
+			UPDATE ip_pool 
+			SET device_id = $1
+			WHERE id = $1`
+	_, err = tx.ExecContext(ctx, query, id)
+	return tx.Commit()
+}
